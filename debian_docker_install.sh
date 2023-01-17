6c04a62392416296b7bdd848059ab7677b5e8590
wiley <throughskybrim@gmail.com>
2023-01-17 22:54:59 +0800

    docker安装脚本

--- debian_docker_install.sh
+++ debian_docker_install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # 安装工具
-apt update
-apt install curl wget vim git -y
+sudo apt update
+sudo apt install curl wget vim git -y
 
 # 安装 docker
-apt remove docker docker-engine docker.io containerd runc
-apt install ca-certificates curl gnupg lsb-release
-mkdir -p /etc/apt/keyrings
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # 安装 docker-compose
-curl -L "https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # 输出版本号
-docker -v
-docker-compose -v
+sudo docker -v
+sudo docker-compose -v
 
