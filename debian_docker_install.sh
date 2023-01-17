a7513cde052096120b00df90b8ced7de4c34740b
wiley <throughskybrim@gmail.com>
2023-01-17 23:03:50 +0800

    docker安装脚本

--- debian_docker_install.sh
+++ debian_docker_install.sh
@@ -10,7 +10,7 @@ echo ">>>>>>移除旧版本<<<<<<"
 sudo apt remove docker docker-engine docker.io containerd runc
 # 设置仓库
 echo ">>>>>>设置repo<<<<<<"
-sudo apt install ca-certificates curl gnupg lsb-release
+sudo apt install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
