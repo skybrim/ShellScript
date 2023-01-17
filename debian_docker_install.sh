89d3e145e4ca7d9e0d4f2269bfb43787e4343813
wiley <throughskybrim@gmail.com>
2023-01-17 22:43:57 +0800

    docker安装脚本

---
+++ debian_docker_install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# 安装工具
+apt update
+apt install curl wget vim git -y
+
+# 安装 docker
+apt remove docker docker-engine docker.io containerd runc
+apt install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# 安装 docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# 输出版本号
+docker -v
+docker-compose -v
+
